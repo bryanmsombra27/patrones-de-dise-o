efc6d7390cfb597a43804b62632c850ec1994d64
bryanmsombra27 <bryanjm96@gmail.com>
2025-10-21 15:09:42 -0600

    factory design

--- 01-creacionales/01-builder.ts
+++ 01-creacionales/01-builder.ts
@@ -1,3 +1,5 @@
+import { COLORS } from "../helpers/colors.ts";
+
 /**
  * ! Patrón Builder:
  * Es un patrón de diseño creacional que nos permite construir objetos complejos
@@ -12,3 +14,74 @@
  *
  * https://refactoring.guru/es/design-patterns/builder
  */
+class Computer {
+  public cpu: string = "cpu - not defined";
+  public ram: string = "ram - not defined";
+  public storage: string = "storage - not defined";
+  public gpu?: string;
+
+  displayConfiguration() {
+    console.log(`Configuración de la computadora:
+
+        CPU: ${this.cpu}
+        RAM: ${this.ram}
+        Almacenamiento: ${
+          this.storage ?? "no tiene almacenamiento integrado pobre"
+        }
+        GPU: ${this.gpu ?? "notiene gpu integrado"}
+        
+        
+        `);
+  }
+}
+
+class ComputerBuilder {
+  private computer: Computer;
+
+  constructor() {
+    this.computer = new Computer();
+  }
+
+  setCPU(cpu: string): ComputerBuilder {
+    this.computer.cpu = cpu;
+    return this;
+  }
+  setRAM(ram: string): ComputerBuilder {
+    this.computer.ram = ram;
+    return this;
+  }
+  setStorage(storage: string): ComputerBuilder {
+    this.computer.storage = storage;
+    return this;
+  }
+  setGPU(gpu: string): ComputerBuilder {
+    this.computer.gpu = gpu;
+    return this;
+  }
+
+  build() {
+    return this.computer;
+  }
+}
+
+function main() {
+  const basicComputer = new ComputerBuilder()
+    .setCPU("KESERO")
+    .setRAM("64 GB")
+    .build();
+
+  //   console.log("%c computadora basica: ", COLORS.blue);
+  //   basicComputer.displayConfiguration();
+
+  const gamingComputer = new ComputerBuilder()
+    .setCPU("INTEL CORE I7 10 GENERATION")
+    .setGPU("INTEL INSIDE CORE E3")
+    .setRAM("128 GB")
+    .setStorage("450 HDD")
+    .build();
+
+  console.log("%c computadora GAMER: ", COLORS.green);
+  gamingComputer.displayConfiguration();
+}
+
+main();
--- 01-creacionales/01.2-builder.ts
+++ 01-creacionales/01.2-builder.ts
@@ -11,7 +11,7 @@
  * * que lo componen.
  */
 
-import { COLORS } from '../helpers/colors.ts';
+import { COLORS } from "../helpers/colors.ts";
 
 //! Tarea: crear un QueryBuilder para construir consultas SQL
 /**
@@ -50,37 +50,60 @@ class QueryBuilder {
   }
 
   select(...fields: string[]): QueryBuilder {
-    throw new Error('Method not implemented.');
+    this.fields = fields;
+    return this;
   }
 
   where(condition: string): QueryBuilder {
-    throw new Error('Method not implemented.');
+    this.conditions.push(condition);
+    return this;
   }
 
-  orderBy(field: string, direction: 'ASC' | 'DESC' = 'ASC'): QueryBuilder {
-    throw new Error('Method not implemented.');
+  orderBy(field: string, direction: "ASC" | "DESC" = "ASC"): QueryBuilder {
+    this.orderFields.push(`ORDER BY ${field} ${direction}`);
+    return this;
   }
 
   limit(count: number): QueryBuilder {
-    throw new Error('Method not implemented.');
+    this.limitCount = count;
+    return this;
   }
 
   execute(): string {
+    //  QUERY ESPERADO
     // Select id, name, email from users where age > 18 and country = 'Cri' order by name ASC limit 10;
-    throw new Error('Method not implemented.');
+
+    const fields =
+      // this.fields.length > 0 ? this.fields.join(", ") : "*";
+      this.fields.length > 0 ? this.fields.toString() : "*";
+
+    const whereClause =
+      this.conditions.length > 0
+        ? `WHERE ${this.conditions.join(" AND ")}`
+        : "";
+    const orderByClause =
+      this.orderFields.length > 0 ? this.orderFields.toString() : "";
+    // this.orderFields.length > 0 ? `ORDER BY ${this.orderFields.join(", ")}`:"";
+
+    const limitClause =
+      this.limitCount && this.limitCount > 0 ? `LIMIT ${this.limitCount}` : "";
+
+    const query = `SELECT  ${fields} FROM ${this.table} ${whereClause} ${orderByClause} ${limitClause}`;
+    return query;
   }
 }
 
 function main() {
-  const usersQuery = new QueryBuilder('users')
-    .select('id', 'name', 'email')
-    .where('age > 18')
+  const usersQuery = new QueryBuilder("users")
+    .select("id", "name", "email")
+    .where("age > 18")
     .where("country = 'Cri'") // Esto debe de hacer una condición AND
-    .orderBy('name', 'ASC')
+    .orderBy("name", "ASC")
+    .orderBy("number", "DESC")
     .limit(10)
     .execute();
 
-  console.log('%cConsulta:\n', COLORS.red);
+  console.log("%cConsulta:\n", COLORS.red);
   console.log(usersQuery);
 }
 
