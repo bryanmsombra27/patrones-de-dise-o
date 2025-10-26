db6e038efb2017092f4ff03ab7285c59107d1d32
bryanmsombra27 <bryanjm96@gmail.com>
2025-10-25 18:40:17 -0600

    adding proxy pattern

--- 02-estructurales/07-proxy.ts
+++ 02-estructurales/07-proxy.ts
@@ -10,3 +10,60 @@
  * https://refactoring.guru/es/design-patterns/proxy
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+class Player {
+  name: string;
+  level: number;
+
+  constructor(name: string, level: number) {
+    this.level = level;
+    this.name = name;
+  }
+}
+
+interface Room {
+  enter(player: Player): void;
+}
+
+class SecretRoom implements Room {
+  enter(player: Player): void {
+    console.log(`%cBienvenido a la sala secreta: ${player.name}`, COLORS.green);
+    console.log("Vas a valer digiverga morro");
+  }
+}
+
+// PROXY
+class MagicPortal implements Room {
+  private room: Room;
+
+  constructor(room: Room) {
+    this.room = room;
+  }
+
+  enter(player: Player): void {
+    if (player.level >= 10) {
+      this.room.enter(player);
+    }
+
+    console.log(
+      "%c Tu nivel no es suficiente para acceder a esta zona",
+      COLORS.red
+    );
+  }
+}
+
+function main() {
+  const player = new Player("koso", 4);
+  const player2 = new Player("keso", 15);
+
+  // ROOMS
+  const secretRoom = new SecretRoom();
+
+  const magicPortalProxy = new MagicPortal(secretRoom);
+  magicPortalProxy.enter(player);
+  magicPortalProxy.enter(player2);
+}
+
+main();
--- 02-estructurales/07.2-proxy.ts
+++ 02-estructurales/07.2-proxy.ts
@@ -11,7 +11,8 @@
  *
  */
 
-import { COLORS } from '../helpers/colors.ts';
+import { timeStamp } from "node:console";
+import { COLORS } from "../helpers/colors.ts";
 
 // 1. Interfaz Document
 interface Document {
@@ -33,24 +34,46 @@ class ConfidentialDocument implements Document {
 
 // 3. Clase Proxy - DocumentProxy
 class DocumentProxy implements Document {
-  private document: ConfidentialDocument;
+  private document: Document;
+
+  // TODO: private musthaveRole :string[]
+  private musthaveRole: string[];
 
   // TODO: Implementar el constructor de la clase DocumentProxy
+  constructor(document: Document, roles: string[] = []) {
+    this.document = document;
+    this.musthaveRole = roles;
+  }
 
   displayContent(user: User): void {
     // TODO: Implementar la lógica para verificar si el usuario tiene permisos
     // Sólo si es admin puede ver el contenido
+    if (this.musthaveRole.includes(user.getRole())) {
+      this.document.displayContent(user);
+      return;
+    }
+
+    // if (user.getRole() === "admin") {
+    // this.document.displayContent(user);
+    //   console.log("USUARIO", user);
+    //   return;
+    // }
+
+    console.log(
+      `%cAcceso denegado. ${user.getName()}, no tienes permisos suficientes para ver este documento.`
+    );
+
     // Caso contrario, mostrar un mensaje de acceso denegado:
-    // EJ: `%cAcceso denegado. ${user.getName()}, no tienes permisos suficientes para ver este documento.`,
+    // EJ: `%cAcceso denegado. ${user.getName()}, no tienes permisos suficientes para ver este documento.%cAcceso denegado. ${user.getName()}, no tienes permisos suficientes para ver este documento.`,
   }
 }
 
 // 4. Clase que representa al Usuario - User
 class User {
   private name: string;
-  private role: 'admin' | 'user';
+  private role: "admin" | "user" | "caja";
 
-  constructor(name: string, role: 'admin' | 'user') {
+  constructor(name: string, role: "admin" | "user" | "caja") {
     this.name = name;
     this.role = role;
   }
@@ -68,18 +91,21 @@ class User {
 
 function main() {
   const confidentialDoc = new ConfidentialDocument(
-    'Este es el contenido confidencial del documento.'
+    "Este es el contenido confidencial del documento."
   );
-  const proxy = new DocumentProxy(confidentialDoc);
+  const proxy = new DocumentProxy(confidentialDoc, ["admin", "caja"]);
 
-  const user1 = new User('Juan', 'user');
-  const user2 = new User('Ana', 'admin');
+  const user1 = new User("Juan", "user");
+  const user2 = new User("Ana", "admin");
+  const user3 = new User("keso", "caja");
 
-  console.log('Intento de acceso del usuario 1:');
+  console.log("Intento de acceso del usuario 1:");
   proxy.displayContent(user1); // Debería denegar el acceso
 
-  console.log('\nIntento de acceso del usuario 2:');
+  console.log("\nIntento de acceso del usuario 2:");
   proxy.displayContent(user2); // Debería permitir el acceso
+  console.log("\nIntento de acceso del usuario 3:");
+  proxy.displayContent(user3); // Debería permitir el acceso
 }
 
 main();
